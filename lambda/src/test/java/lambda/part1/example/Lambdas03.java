958b2d5d5695d09773fa9a504576d336f5a15c34
brainydamage@gmail.com <gonduras_14>
2017-10-02 17:10:51 +0300

    refactoring

--- lambda/src/test/java/lambda/part1/example/Lambdas03.java
+++ lambda/src/test/java/lambda/part1/example/Lambdas03.java
@@ -67,7 +67,7 @@ public class Lambdas03 {
 
     private final String delimiter = "-";
 
-    //for non-static method
+    //non-static method
     private String stringSumWithDelimiter(String s1, String s2) {
         return s1 + delimiter + s2;
     }
