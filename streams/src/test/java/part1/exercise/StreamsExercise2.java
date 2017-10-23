5c00dae625d61dcda22c4c63ba75c07f4072e598
Fedor Fedichkin <f.fedichkin@gmail.com>
2017-10-23 17:59:33 +0300

    method greatestExperiencePerEmployer in class StreamsExercise2 is done

--- streams/src/test/java/part1/exercise/StreamsExercise2.java
+++ streams/src/test/java/part1/exercise/StreamsExercise2.java
@@ -3,7 +3,6 @@ package part1.exercise;
 import data.Employee;
 import data.JobHistoryEntry;
 import data.Person;
-import javafx.util.Pair;
 import org.junit.Test;
 
 import java.util.*;
@@ -14,11 +13,221 @@ import static org.junit.Assert.assertEquals;
 /**
  * @see <a href="https://youtu.be/kxgo7Y4cdA8">Через тернии к лямбдам, часть 1</a>
  * @see <a href="https://youtu.be/JRBWBJ6S4aU">Через тернии к лямбдам, часть 2</a>
- *
  * @see <a href="https://youtu.be/O8oN4KSZEXE">Stream API, часть 1</a>
  * @see <a href="https://youtu.be/i0Jr2l3jrDA">Stream API, часть 2</a>
  */
 public class StreamsExercise2 {
+
+    // TODO // done
+    @Test
+    public void employersStuffList() {
+        List<Employee> employees = getEmployees();
+        Map<String, Set<Person>> result = employees.stream()
+                .flatMap(employee -> employee.getJobHistory().stream()
+                        .map(jobHistoryEntry -> new PersonEmployerPair(employee.getPerson(), jobHistoryEntry.getEmployer())))
+                .collect(Collectors.groupingBy(classifier -> classifier.getEmployer(),
+                        Collectors.mapping(mapper -> mapper.getPerson(), Collectors.toSet())));
+
+        Map<String, Set<Person>> expected = new HashMap<>();
+        expected.put("epam", new HashSet<>(Arrays.asList(
+                new Person("John", "Galt", 20),
+                new Person("John", "Doe", 21),
+                new Person("John", "White", 22),
+                new Person("John", "Galt", 23),
+                new Person("John", "Doe", 24),
+                new Person("John", "White", 25),
+                new Person("John", "Galt", 26),
+                new Person("Bob", "Doe", 27),
+                new Person("John", "White", 28),
+                new Person("John", "Galt", 29),
+                new Person("John", "Doe", 30),
+                new Person("Bob", "White", 31))));
+        expected.put("google", new HashSet<>(Arrays.asList(
+                new Person("John", "Galt", 20),
+                new Person("John", "Galt", 23),
+                new Person("John", "Galt", 26),
+                new Person("John", "Galt", 29)
+        )));
+        expected.put("yandex", new HashSet<>(Arrays.asList(
+                new Person("John", "Doe", 21),
+                new Person("John", "Doe", 24),
+                new Person("Bob", "Doe", 27),
+                new Person("John", "Doe", 30)
+        )));
+        expected.put("abc", new HashSet<>(Arrays.asList(
+                new Person("John", "Doe", 21),
+                new Person("John", "Doe", 24),
+                new Person("Bob", "Doe", 27),
+                new Person("John", "Doe", 30)
+        )));
+        assertEquals(expected, result);
+    }
+
+    /**
+     * Преобразовать список сотрудников в отображение [компания -> множество людей, начавших свою карьеру в этой компании].
+     * <p>
+     * Пример.
+     * <p>
+     * Входные данные:
+     * Employees = [
+     * Employee_1 = {
+     * Person = {"Alex", "Doe", 32},
+     * JobHistory = [
+     * {2, "dev", "epam"},
+     * {3, "dev", "yandex"},
+     * {1, "dev", "mail"},
+     * ]
+     * },
+     * Employee_2 = {
+     * Person = {"Adam", "Smith", 25},
+     * JobHistory = [
+     * {1, "QA", "yandex"},
+     * {2, "QA", "mail"},
+     * ]
+     * },
+     * Employee_3 = {
+     * Person = {"John", "Galt", 27},
+     * JobHistory = [
+     * {1, "QA", "epam"},
+     * {3, "dev", "epam"},
+     * ]
+     * }
+     * ]
+     * <p>
+     * Выходные данные:
+     * Result = [
+     * "epam" -> [
+     * {"Alex", "Doe", 32},
+     * {"John", "Galt", 27}
+     * ],
+     * "yandex" -> [
+     * {"Adam", "Smith", 25}
+     * ]
+     * ]
+     */
+
+    // TODO // done
+    @Test
+    public void indexByFirstEmployer() {
+        List<Employee> employees = getEmployees();
+        Map<String, Set<Person>> result = employees.stream()
+                .flatMap(employee -> employee.getJobHistory().stream()
+                        .limit(1)
+                        .map(jobHistoryEntry -> new PersonEmployerPair(employee.getPerson(), jobHistoryEntry.getEmployer())))
+                .collect(Collectors.groupingBy(classifier -> classifier.getEmployer(),
+                        Collectors.mapping(mapper -> mapper.getPerson(), Collectors.toSet())));
+
+        Map<String, Set<Person>> expected = new HashMap<>();
+        expected.put("epam", new HashSet<>(Arrays.asList(
+                new Person("John", "Galt", 20),
+                new Person("John", "White", 22),
+                new Person("John", "Galt", 23),
+                new Person("John", "White", 25),
+                new Person("John", "Galt", 26),
+                new Person("John", "White", 28),
+                new Person("John", "Galt", 29),
+                new Person("Bob", "White", 31)
+        )));
+        expected.put("yandex", new HashSet<>(Arrays.asList(
+                new Person("John", "Doe", 21),
+                new Person("John", "Doe", 24),
+                new Person("Bob", "Doe", 27),
+                new Person("John", "Doe", 30)
+        )));
+        assertEquals(expected, result);
+    }
+
+    /**
+     * Преобразовать список сотрудников в отображение [компания -> сотрудник, суммарно проработавший в ней наибольшее время].
+     * Гарантируется, что такой сотрудник будет один.
+     */
+
+    // TODO // done
+    @Test
+    public void greatestExperiencePerEmployer() {
+        List<Employee> employees = getEmployees();
+        Map<String, Person> result = employees.stream()
+                .flatMap(employee -> employee.getJobHistory().stream()
+                        .collect(Collectors.groupingBy(JobHistoryEntry::getEmployer, Collectors.summingInt(JobHistoryEntry::getDuration)))
+                        .entrySet().stream()
+                        .map(pair -> new PersonEmployerWithDuration(employee.getPerson(), pair.getKey(), pair.getValue())))
+                .collect(Collectors.groupingBy(PersonEmployerWithDuration::getEmployer,
+                        Collectors.collectingAndThen(Collectors.maxBy(Comparator.comparing(PersonEmployerWithDuration::getDuration)),
+                                optional -> optional.get().getPerson())));
+
+        Map<String, Person> expected = new HashMap<>();
+        expected.put("epam", new Person("John", "White", 28));
+        expected.put("google", new Person("John", "Galt", 29));
+        expected.put("yandex", new Person("John", "Doe", 30));
+        expected.put("abc", new Person("John", "Doe", 30));
+        assertEquals(expected, result);
+    }
+
+    private List<Employee> getEmployees() {
+        return Arrays.asList(
+                new Employee("John", "Galt", 20,
+                        Arrays.asList(
+                                new JobHistoryEntry(3, "dev", "epam"),
+                                new JobHistoryEntry(2, "dev", "google")
+                        )),
+                new Employee("John", "Doe", 21,
+                        Arrays.asList(
+                                new JobHistoryEntry(4, "BA", "yandex"),
+                                new JobHistoryEntry(2, "QA", "epam"),
+                                new JobHistoryEntry(2, "dev", "abc")
+                        )),
+                new Employee("John", "White", 22,
+                        Collections.singletonList(
+                                new JobHistoryEntry(6, "QA", "epam")
+                        )),
+                new Employee("John", "Galt", 23,
+                        Arrays.asList(
+                                new JobHistoryEntry(3, "dev", "epam"),
+                                new JobHistoryEntry(2, "dev", "google")
+                        )),
+                new Employee("John", "Doe", 24,
+                        Arrays.asList(
+                                new JobHistoryEntry(4, "QA", "yandex"),
+                                new JobHistoryEntry(2, "BA", "epam"),
+                                new JobHistoryEntry(2, "dev", "abc")
+                        )),
+                new Employee("John", "White", 25,
+                        Collections.singletonList(
+                                new JobHistoryEntry(6, "QA", "epam")
+                        )),
+                new Employee("John", "Galt", 26,
+                        Arrays.asList(
+                                new JobHistoryEntry(3, "dev", "epam"),
+                                new JobHistoryEntry(1, "dev", "google")
+                        )),
+                new Employee("Bob", "Doe", 27,
+                        Arrays.asList(
+                                new JobHistoryEntry(4, "QA", "yandex"),
+                                new JobHistoryEntry(2, "QA", "epam"),
+                                new JobHistoryEntry(2, "QA", "abc"),
+                                new JobHistoryEntry(2, "dev", "abc")
+                        )),
+                new Employee("John", "White", 28,
+                        Collections.singletonList(
+                                new JobHistoryEntry(8, "BA", "epam")
+                        )),
+                new Employee("John", "Galt", 29,
+                        Arrays.asList(
+                                new JobHistoryEntry(3, "dev", "epam"),
+                                new JobHistoryEntry(3, "dev", "google")
+                        )),
+                new Employee("John", "Doe", 30,
+                        Arrays.asList(
+                                new JobHistoryEntry(5, "QA", "yandex"),
+                                new JobHistoryEntry(2, "QA", "epam"),
+                                new JobHistoryEntry(5, "dev", "abc")
+                        )),
+                new Employee("Bob", "White", 31,
+                        Collections.singletonList(
+                                new JobHistoryEntry(6, "QA", "epam")
+                        ))
+        );
+    }
 
     /**
      * Преобразовать список сотрудников в отображение [компания -> множество людей, когда-либо работавших в этой компании].
@@ -72,6 +281,31 @@ public class StreamsExercise2 {
         private Person person;
         private String employer;
 
+        public PersonEmployerPair(Person person, String employer) {
+            this.person = person;
+            this.employer = employer;
+        }
+
+        public Person getPerson() {
+            return person;
+        }
+
+        public String getEmployer() {
+            return employer;
+        }
+    }
+
+    private static class PersonEmployerWithDuration {
+        private Person person;
+        private String employer;
+        private int duration;
+
+        public PersonEmployerWithDuration(Person person, String employer, int duration) {
+            this.person = person;
+            this.employer = employer;
+            this.duration = duration;
+        }
+
         public Person getPerson() {
             return person;
         }
@@ -80,213 +314,8 @@ public class StreamsExercise2 {
             return employer;
         }
 
-        public PersonEmployerPair(Person person, String employer) {
-            this.person = person;
-            this.employer = employer;
+        public int getDuration() {
+            return duration;
         }
     }
-
-    // TODO // done
-    @Test
-    public void employersStuffList() {
-        List<Employee> employees = getEmployees();
-        Map<String, Set<Person>> result = employees.stream()
-                .flatMap(employee -> employee.getJobHistory().stream()
-                        .map(jobHistoryEntry -> new PersonEmployerPair(employee.getPerson(), jobHistoryEntry.getEmployer())))
-                .collect(Collectors.groupingBy(classifier -> classifier.getEmployer(),
-                        Collectors.mapping(mapper -> mapper.getPerson(), Collectors.toSet())));
-
-        Map<String, Set<Person>> expected = new HashMap<>();
-        expected.put("epam", new HashSet<>(Arrays.asList(
-                new Person("John", "Galt", 20),
-                new Person("John", "Doe", 21),
-                new Person("John", "White", 22),
-                new Person("John", "Galt", 23),
-                new Person("John", "Doe", 24),
-                new Person("John", "White", 25),
-                new Person("John", "Galt", 26),
-                new Person("Bob", "Doe", 27),
-                new Person("John", "White", 28),
-                new Person("John", "Galt", 29),
-                new Person("John", "Doe", 30),
-                new Person("Bob", "White", 31))));
-        expected.put("google", new HashSet<>(Arrays.asList(
-                new Person("John", "Galt", 20),
-                new Person("John", "Galt", 23),
-                new Person("John", "Galt", 26),
-                new Person("John", "Galt", 29)
-        )));
-        expected.put("yandex", new HashSet<>(Arrays.asList(
-                new Person("John", "Doe", 21),
-                new Person("John", "Doe", 24),
-                new Person("Bob", "Doe", 27),
-                new Person("John", "Doe", 30)
-        )));
-        expected.put("abc", new HashSet<>(Arrays.asList(
-                new Person("John", "Doe", 21),
-                new Person("John", "Doe", 24),
-                new Person("Bob", "Doe", 27),
-                new Person("John", "Doe", 30)
-        )));
-        assertEquals(expected, result);
-    }
-
-    /**
-     * Преобразовать список сотрудников в отображение [компания -> множество людей, начавших свою карьеру в этой компании].
-     *
-     * Пример.
-     *
-     * Входные данные:
-     * Employees = [
-     *    Employee_1 = {
-     *        Person = {"Alex", "Doe", 32},
-     *        JobHistory = [
-     *           {2, "dev", "epam"},
-     *           {3, "dev", "yandex"},
-     *           {1, "dev", "mail"},
-     *        ]
-     *    },
-     *    Employee_2 = {
-     *        Person = {"Adam", "Smith", 25},
-     *        JobHistory = [
-     *           {1, "QA", "yandex"},
-     *           {2, "QA", "mail"},
-     *        ]
-     *    },
-     *    Employee_3 = {
-     *        Person = {"John", "Galt", 27},
-     *        JobHistory = [
-     *           {1, "QA", "epam"},
-     *           {3, "dev", "epam"},
-     *        ]
-     *    }
-     * ]
-     *
-     * Выходные данные:
-     * Result = [
-     *    "epam" -> [
-     *       {"Alex", "Doe", 32},
-     *       {"John", "Galt", 27}
-     *    ],
-     *    "yandex" -> [
-     *       {"Adam", "Smith", 25}
-     *    ]
-     * ]
-     */
-
-    // TODO // done
-    @Test
-    public void indexByFirstEmployer() {
-        List<Employee> employees = getEmployees();
-        Map<String, Set<Person>> result = employees.stream()
-                .flatMap(employee -> employee.getJobHistory().stream()
-                        .limit(1)
-                        .map(jobHistoryEntry -> new PersonEmployerPair(employee.getPerson(), jobHistoryEntry.getEmployer())))
-                .collect(Collectors.groupingBy(classifier -> classifier.getEmployer(),
-                        Collectors.mapping(mapper -> mapper.getPerson(), Collectors.toSet())));
-
-
-        Map<String, Set<Person>> expected = new HashMap<>();
-        expected.put("epam", new HashSet<>(Arrays.asList(
-                new Person("John", "Galt", 20),
-                new Person("John", "White", 22),
-                new Person("John", "Galt", 23),
-                new Person("John", "White", 25),
-                new Person("John", "Galt", 26),
-                new Person("John", "White", 28),
-                new Person("John", "Galt", 29),
-                new Person("Bob", "White", 31)
-        )));
-        expected.put("yandex", new HashSet<>(Arrays.asList(
-                new Person("John", "Doe", 21),
-                new Person("John", "Doe", 24),
-                new Person("Bob", "Doe", 27),
-                new Person("John", "Doe", 30)
-        )));
-        assertEquals(expected, result);
-    }
-
-    /**
-     * Преобразовать список сотрудников в отображение [компания -> сотрудник, суммарно проработавший в ней наибольшее время].
-     * Гарантируется, что такой сотрудник будет один.
-     */
-    @Test
-    public void greatestExperiencePerEmployer() {
-        Map<String, Person> result = null;// TODO
-
-        Map<String, Person> expected = new HashMap<>();
-        expected.put("epam", new Person("John", "White", 28));
-        expected.put("google", new Person("John", "Galt", 29));
-        expected.put("yandex", new Person("John", "Doe", 30));
-        expected.put("abc", new Person("John", "Doe", 30));
-        assertEquals(expected, result);
-    }
-
-
-    private List<Employee> getEmployees() {
-        return Arrays.asList(
-            new Employee("John", "Galt", 20,
-                    Arrays.asList(
-                            new JobHistoryEntry(3, "dev", "epam"),
-                            new JobHistoryEntry(2, "dev", "google")
-                    )),
-            new Employee("John", "Doe", 21,
-                    Arrays.asList(
-                            new JobHistoryEntry(4, "BA", "yandex"),
-                            new JobHistoryEntry(2, "QA", "epam"),
-                            new JobHistoryEntry(2, "dev", "abc")
-                    )),
-            new Employee("John", "White", 22,
-                    Collections.singletonList(
-                            new JobHistoryEntry(6, "QA", "epam")
-                    )),
-            new Employee("John", "Galt", 23,
-                    Arrays.asList(
-                            new JobHistoryEntry(3, "dev", "epam"),
-                            new JobHistoryEntry(2, "dev", "google")
-                    )),
-            new Employee("John", "Doe", 24,
-                    Arrays.asList(
-                            new JobHistoryEntry(4, "QA", "yandex"),
-                            new JobHistoryEntry(2, "BA", "epam"),
-                            new JobHistoryEntry(2, "dev", "abc")
-                    )),
-            new Employee("John", "White", 25,
-                    Collections.singletonList(
-                            new JobHistoryEntry(6, "QA", "epam")
-                    )),
-            new Employee("John", "Galt", 26,
-                    Arrays.asList(
-                            new JobHistoryEntry(3, "dev", "epam"),
-                            new JobHistoryEntry(1, "dev", "google")
-                    )),
-            new Employee("Bob", "Doe", 27,
-                    Arrays.asList(
-                            new JobHistoryEntry(4, "QA", "yandex"),
-                            new JobHistoryEntry(2, "QA", "epam"),
-                            new JobHistoryEntry(2, "QA", "abc"),
-                            new JobHistoryEntry(2, "dev", "abc")
-                    )),
-            new Employee("John", "White", 28,
-                    Collections.singletonList(
-                            new JobHistoryEntry(8, "BA", "epam")
-                    )),
-            new Employee("John", "Galt", 29,
-                    Arrays.asList(
-                            new JobHistoryEntry(3, "dev", "epam"),
-                            new JobHistoryEntry(3, "dev", "google")
-                    )),
-            new Employee("John", "Doe", 30,
-                    Arrays.asList(
-                            new JobHistoryEntry(5, "QA", "yandex"),
-                            new JobHistoryEntry(2, "QA", "epam"),
-                            new JobHistoryEntry(5, "dev", "abc")
-                    )),
-            new Employee("Bob", "White", 31,
-                    Collections.singletonList(
-                            new JobHistoryEntry(6, "QA", "epam")
-                    ))
-        );
-    }
-
 }
